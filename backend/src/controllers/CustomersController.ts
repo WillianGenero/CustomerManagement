e3227da410161679f729f0efc586eb42863e410a
Willian Genero <willian-b-g@hotmail.com>
2020-08-04 18:50:35 -0300

    Criando clientes com seus respectivos endereços

--- backend/src/controllers/CustomersController.ts
+++ backend/src/controllers/CustomersController.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from 'express'
 import knex from '../database/connection'
 
+interface address {
+  cep: string,
+  city: string,
+  state: string,
+  district: string,
+  street: string,
+  number: number,
+  complement: string,
+  type: number,
+}
+
 const CustomersController = {
   async index (request: Request, response: Response) {
     const user = await knex('customers').select('*')
@@ -9,7 +20,7 @@ const CustomersController = {
   },
 
   async create (request: Request, response: Response) {
-    const { name, cpf, email, phone, cep, city, state, district, street, number, complement, typeAddress } = request.body
+    const { name, cpf, email, phone, addresses } = request.body
 
     const customer = {
       name, cpf, email, phone
@@ -17,17 +28,38 @@ const CustomersController = {
 
     const trx = await knex.transaction()
 
-    const insertedId = await trx('customers').
-      insert(customer).
-      returning('id').
-      catch((error) => {
-        return response.json(error)
+    const customer_id = await trx('customers')
+      .insert(customer)
+      .returning('id')
+      .then(resp => {
+        addresses.map(async (address: address) => {
+          const local = {
+            cep: address.cep,
+            city: address.city,
+            state: address.state,
+            district: address.district,
+            street: address.street,
+            number: address.number,
+            complement: address.complement,
+            typeAddress: address.type,
+            customer_id: resp[0],
+          }
+          await knex('address')
+            .insert(local)
+            .catch((error) => {
+              trx.rollback()
+              return response.status(400).json(error)
+            })
+        })
+        trx.commit()
+      })
+      .catch((error) => {
+        trx.rollback()
+        return response.status(400).json(error)
       })
 
-    trx.commit()
-
-    return response.json({ insertedId })
-  }
+    return response.json({ customer_id })
+  },
 }
 
 export default CustomersController
